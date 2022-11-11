b106b152166147554b8437eea32c2db6c0651dc3
Atul <atulkhajuria234@gmail.com>
2022-11-11 20:20:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const dynamicContent = document.getElementById("dynamic-text");
+
+const phrases = ["an IT Engineer...", "a Programmer...", "a Human Being..."];
+
+let letterIndex = 0;
+let phraseIndex = 0;
+let typingSpeed = 200;
+let erasingSpeed = 150;
+
+printLetters(phrases[phraseIndex]);
+
+function printLetters(phrase) {
+  if (letterIndex == phrase.length) {
+    //clear letters which have been typed
+    clearLetters();
+  } else if (letterIndex < phrase.length) {
+    dynamicContent.textContent += phrase.charAt(letterIndex);
+    letterIndex += 1;
+    setTimeout(function () {
+      printLetters(phrase);
+    }, typingSpeed);
+  }
+}
+function clearLetters() {
+  if (letterIndex == -1) {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    letterIndex = 0;
+    printLetters(phrases[phraseIndex]);
+  } else if (letterIndex > -1) {
+    let updatedPhrase = "";
+    for (let index = 0; index < letterIndex; index++) {
+      updatedPhrase += phrases[phraseIndex].charAt(index);
+    }
+    dynamicContent.textContent = updatedPhrase;
+    // console.log(updatedPhrase);
+    letterIndex -= 1;
+    setTimeout(clearLetters, erasingSpeed);
+  }
+}
