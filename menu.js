ccc4060aa154476dd8d5db8caab920f01f737f84
Atul <atulkhajuria234@gmail.com>
2022-11-17 19:02:52 +0530

    added the open and close menu button for small screen

--- menu.js
+++ menu.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("menu-button").addEventListener("click", showMenu);
-  document.getElementById("close-menu").addEventListener("click", closeMenu);
-
   function showMenu() {
     const menu = document.querySelector("section.menu");
     menu.style.height = "100%";
@@ -10,4 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const menu = document.querySelector("section.menu");
     menu.style.height = "0";
   }
+
+  function addClickToMenuItems() {
+    const menuItems = document.querySelectorAll("section.menu nav ul>li");
+    for (let menuItem of menuItems) {
+      menuItem.addEventListener("click", closeMenu);
+    }
+  }
+  document.getElementById("menu-button").addEventListener("click", showMenu);
+  document.getElementById("close-menu").addEventListener("click", closeMenu);
+  addClickToMenuItems();
 });
